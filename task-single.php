6f89c880c7919a43e4f084549435f85beeb92071
ferezecarr <ferezecarr@gmail.com>
2018-08-20 09:56:19 -0300

    App de tareas utilizando php y ajax

---
+++ task-single.php
@@ -0,0 +1,25 @@
+<?php
+
+    include('database.php');
+
+    $id = $_POST['id'];
+    $query = "SELECT * FROM task WHERE id = $id";
+    $result = mysqli_query($connection , $query);
+    if(!$result) {
+        die('Query Failed');
+    }
+
+    $json = array();
+    while($row = mysqli_fetch_array($result)) {
+        $json = array(
+            'name' => $row['name'],
+            'description' => $row['description'],
+            'id' => $row['id']
+        );
+    }
+
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
+
+
+?>
